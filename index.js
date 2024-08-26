7a2d754ed008dd47cf972d936888c2e03b737622
suneel reddy <suneelreddy912@gmail.com>
2024-08-26 09:53:17 +0530

    setting up the view engine as the ejs

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const port=8000;
 
 //use router from the routes
 
-app.use('/',require('./routes/index'));
+app.use('/',require('./routes'));
+//setting up the vies engine 
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
